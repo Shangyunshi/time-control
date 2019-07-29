f4f50935f015cda0180cedab7a204e1a3e6b755f
时建平 <shijianping@192.168.0.105>
2019-07-29 21:51:14 +0800

    first commit

---
+++ app/src/main/java/com/shangyunshi/timecontrol/AuthorizationManagementActivity.java
@@ -0,0 +1,16 @@
+package com.shangyunshi.timecontrol;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//1.绘制权限界面
+//2.java 关键字
+public class AuthorizationManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization_management);
+    }
+}
